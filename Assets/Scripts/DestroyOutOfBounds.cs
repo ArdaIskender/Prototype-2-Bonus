5beecb77d32f00b4d8547456f0a9197307ad5bbb
Arda Iskender <iskenderarda@gmail.com>
2022-11-28 05:32:29 +0300

    4-Hard: Game User Interface (completed)
    -Added GameManager.
    -The player has three lives and loses every time an animal goes out of bounds or the player collides with the animals.

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -6,10 +6,11 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
     private float horizontalBound = -20f;
     private float verticalBound = -7f;
+    private GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -17,18 +18,22 @@ public class DestroyOutOfBounds : MonoBehaviour
     {
         if (transform.position.x < horizontalBound)
         {
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
         if (transform.position.x > -horizontalBound)
         {
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
         if (transform.position.z < verticalBound)
         {
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
         if (transform.position.z > 20f)
         {
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
     }
